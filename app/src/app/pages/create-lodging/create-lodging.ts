9aca014ed39816b29466d6f4b5221ce3a999de33
David8633 <davidinstituto282@gmail.com>
2026-02-20 00:13:53 +0100

    subida de las posibles rutas que tendra la app, duda en la parte de detalles que no saber como hacerlo para que no se vea el id, posible solucion de que no se vea o que al intentar cambiarlo que no te deje y se quede ahi en ese id para siempre

---
+++ app/src/app/pages/create-lodging/create-lodging.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-lodging',
+  imports: [],
+  templateUrl: './create-lodging.html',
+  styleUrl: './create-lodging.css',
+})
+export class CreateLodging {
+
+}
